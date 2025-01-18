75336f19456eb5c2905ef1c2a5d44900c0908045
LahiruMadushann <lahirumadushandl@gmail.com>
2025-01-18 21:30:38 +0530

    implement api for get roles by role category

--- src/main/java/com/project_management/repositories/ProjectRepository.java
+++ src/main/java/com/project_management/repositories/ProjectRepository.java
@@ -1,8 +1,13 @@
 package com.project_management.repositories;
 
+import com.project_management.models.PerfectEmployee;
 import com.project_management.models.Project;
+import com.project_management.models.enums.RoleCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {}
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
--- src/main/java/com/project_management/services/PerfectRoleService.java
+++ src/main/java/com/project_management/services/PerfectRoleService.java
@@ -1,11 +1,14 @@
 package com.project_management.services;
 
+import com.project_management.models.PerfectEmployee;
 import com.project_management.models.PerfectRole;
+import com.project_management.models.enums.RoleCategory;
 
 import java.util.List;
 
 public interface PerfectRoleService {
     PerfectRole saveRole(String roleName);
     List<PerfectRole> getAllRoles();
+    List<PerfectEmployee>  getAllRolesCategory(RoleCategory roleCategory);
 }
 
